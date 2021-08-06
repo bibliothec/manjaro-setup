c3e05034b9114e281eb091b7b66158faa1d178f0
rune <mupimupicandy@gmail.com>
2021-08-06 19:11:17 +0900

    2021/8/6

---
+++ manjaro_setup.sh
@@ -0,0 +1,3 @@
+!#/bin/bash
+
+sudo pacman -S lxtask blender krita firefox-developer-edition opentoonz kdenlive ardour godot arduino cura gimp inkscape libreoffice virtualbox discord code brave wine vim emacs nano micro atom tor winetricks obs-studio qbittorrent musescore lmms shotcut darktable audacity atom unar wireshark
