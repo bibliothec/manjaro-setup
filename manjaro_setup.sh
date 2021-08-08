12577027dc7834539abe4ef999c3a41b7dbc5203
rune <mupimupicandy@gmail.com>
2021-08-08 20:05:58 +0900

    20201/8/8

--- manjaro_setup.sh
+++ manjaro_setup.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Syu
 
-sudo pacman -S lxtask blender krita firefox-developer-edition opentoonz kdenlive ardour godot arduino cura gimp inkscape libreoffice virtualbox discord code brave wine vim emacs nano micro atom torbrowser-launcher winetricks obs-studio qbittorrent musescore lmms shotcut darktable audacity atom wireshark-qt cpu-x qalculate-gtk gnome-terminal kdeconnect yay git gcc clang go python rust net-tools base-devel qtcreator intellij-idea-community-edition pycharm-community-edition calibre vlc vbam-wx desmume ppsspp opencv gtk3 appimagelauncher chromium texlive-langjapanese texlive-most texworks latte-dock pcsx2 dolphin-emu neofetch snes9x-gtk xournalpp
+sudo pacman -S lxtask blender krita firefox-developer-edition opentoonz kdenlive ardour godot arduino cura gimp inkscape libreoffice virtualbox discord code brave wine vim emacs nano micro atom torbrowser-launcher winetricks obs-studio qbittorrent musescore lmms shotcut darktable audacity atom wireshark-qt cpu-x qalculate-gtk gnome-terminal kdeconnect yay git gcc clang go python rust net-tools base-devel qtcreator intellij-idea-community-edition pycharm-community-edition calibre vlc vbam-wx desmume ppsspp opencv gtk3 appimagelauncher chromium texlive-langjapanese texlive-most texworks latte-dock pcsx2 dolphin-emu neofetch snes9x-gtk xournalpp mariadb docker
 
 sudo pacman -S noto-fonts-cjk plasma-wayland-session
 
