4f742fdb3e75add06b733be73353e7b8e14f8093
rune <mupimupicandy@gmail.com>
2021-08-08 19:51:46 +0900

    2021/8/8

--- manjaro_setup.sh
+++ manjaro_setup.sh
@@ -1,3 +1,9 @@
-!#/bin/bash
+#!/bin/bash
 
-sudo pacman -S lxtask blender krita firefox-developer-edition opentoonz kdenlive ardour godot arduino cura gimp inkscape libreoffice virtualbox discord code brave wine vim emacs nano micro atom tor winetricks obs-studio qbittorrent musescore lmms shotcut darktable audacity atom unar wireshark
+sudo pacman -Syu
+
+sudo pacman -S lxtask blender krita firefox-developer-edition opentoonz kdenlive ardour godot arduino cura gimp inkscape libreoffice virtualbox discord code brave wine vim emacs nano micro atom torbrowser-launcher winetricks obs-studio qbittorrent musescore lmms shotcut darktable audacity atom wireshark-qt cpu-x qalculate-gtk gnome-terminal kdeconnect yay git gcc clang go python rust net-tools base-devel qtcreator intellij-idea-community-edition pycharm-community-edition calibre vlc vbam-wx desmume ppsspp opencv gtk3 appimagelauncher chromium texlive-langjapanese texlive-most texworks latte-dock pcsx2 dolphin-emu neofetch snes9x-gtk xournalpp
+
+sudo pacman -S noto-fonts-cjk plasma-wayland-session
+
+yay pacamn -S olive zrythm rpcs3 sendanywhere meshroom epsxe visual-studio-code-bin
