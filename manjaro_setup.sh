0272d1af6c202b4561a7d026e240acf16407ac14
bibliothec <mupimupicandy@gmail.com>
2021-08-24 20:04:43 +0900

    add some

--- manjaro_setup.sh
+++ manjaro_setup.sh
@@ -2,8 +2,12 @@
 
 sudo pacman -Syu
 
-sudo pacman -S lxtask blender krita firefox-developer-edition opentoonz kdenlive ardour godot arduino cura gimp inkscape libreoffice virtualbox discord code brave wine vim emacs nano micro atom torbrowser-launcher winetricks obs-studio qbittorrent musescore lmms shotcut darktable audacity atom wireshark-qt cpu-x qalculate-gtk gnome-terminal kdeconnect yay git gcc clang go python rust net-tools base-devel qtcreator intellij-idea-community-edition pycharm-community-edition calibre vlc vbam-wx desmume ppsspp opencv gtk3 appimagelauncher chromium texlive-langjapanese texlive-most texworks latte-dock pcsx2 dolphin-emu neofetch snes9x-gtk xournalpp mariadb docker
+sudo pacman -S lxtask blender krita firefox-developer-edition opentoonz kdenlive ardour godot arduino cura gimp inkscape libreoffice virtualbox discord code brave wine vim emacs nano micro atom torbrowser-launcher winetricks obs-studio qbittorrent musescore lmms shotcut darktable audacity atom wireshark-qt cpu-x qalculate-gtk gnome-terminal kdeconnect yay git gcc clang go python rust net-tools base-devel qtcreator intellij-idea-community-edition pycharm-community-edition calibre vlc vbam-wx desmume ppsspp opencv gtk3 appimagelauncher chromium texlive-langjapanese texlive-most texworks latte-dock pcsx2 dolphin-emu neofetch snes9x-gtk xournalpp mariadb docker jupyter-notebook mupen64plus rpi-imager alacritty tree playonlinux
 
-sudo pacman -S noto-fonts-cjk plasma-wayland-session
+sudo pacman -S noto-fonts-cjk plasma-wayland-session 
 
-yay pacamn -S olive zrythm rpcs3 sendanywhere meshroom epsxe visual-studio-code-bin
+yay pacamn -S olive zrythm-git rpcs3 sendanywhere meshroom epsxe visual-studio-code-bin android-studio
+
+sudo snap install citra-emu yuzu
+
+pip install numpy pandas tensorflow scikit-learn matplotlib pillow openpyxl scrapy selenium Scrapy requests beautifulsoup4 flask pygame opencv-python
