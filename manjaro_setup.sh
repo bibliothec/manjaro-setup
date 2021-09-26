92311c55bba270eb972b5fba98065478dcc2b148
bibliothec <mupimupicandy@gmail.com>
2021-09-26 12:26:19 +0900

    add some

--- manjaro_setup.sh
+++ manjaro_setup.sh
@@ -3,10 +3,10 @@
 sudo pacman -Syu
 yay -Syu
 
-sudo pacman -S --noconfirm lxtask blender krita firefox-developer-edition opentoonz kdenlive ardour godot arduino cura gimp inkscape libreoffice virtualbox discord code brave wine vim emacs nano micro atom torbrowser-launcher winetricks obs-studio qbittorrent musescore lmms shotcut darktable audacity atom wireshark-qt cpu-x qalculate-gtk gnome-terminal kdeconnect yay git gcc clang go python rust net-tools base-devel qtcreator intellij-idea-community-edition pycharm-community-edition calibre vlc vbam-wx desmume ppsspp opencv gtk3 appimagelauncher chromium texlive-langjapanese texlive-most texworks latte-dock pcsx2 dolphin-emu neofetch snes9x-gtk xournalpp mariadb docker jupyter-notebook mupen64plus rpi-imager alacritty tree playonlinux rawtherapee youtube-dl ruby cuda mumble notepadqq 
+sudo pacman -S --noconfirm lxtask blender krita firefox-developer-edition opentoonz kdenlive ardour godot arduino cura gimp inkscape libreoffice virtualbox discord code brave wine vim emacs nano micro atom torbrowser-launcher winetricks obs-studio qbittorrent musescore lmms shotcut darktable audacity atom wireshark-qt cpu-x qalculate-gtk gnome-terminal kdeconnect yay git gcc clang go python rust net-tools base-devel qtcreator intellij-idea-community-edition pycharm-community-edition calibre vlc vbam-wx desmume ppsspp opencv gtk3 appimagelauncher chromium texlive-langjapanese texlive-most texworks latte-dock pcsx2 dolphin-emu neofetch snes9x-gtk xournalpp mariadb docker jupyter-notebook mupen64plus rpi-imager alacritty tree playonlinux rawtherapee youtube-dl ruby cuda mumble notepadqq scribus
 
 sudo pacman -S --noconfirm noto-fonts-cjk plasma-wayland-session 
 
-yay -S --noconfirm olive zrythm-git rpcs3 sendanywhere meshroom epsxe visual-studio-code-bin android-studio yuzu-mainline-bin meshroom-bin citra-qt-bin scilab-bin youtube-dl-gui-git unreal-engine vita3k-git reaper-bin google-chrome  local-by-flywheel-bin
+yay -S --noconfirm olive zrythm-git rpcs3 sendanywhere meshroom epsxe visual-studio-code-bin android-studio yuzu-mainline-bin meshroom-bin citra-qt-bin scilab-bin youtube-dl-gui-git unreal-engine vita3k-git reaper-bin google-chrome  local-by-flywheel-bin github-desktop aptpac
 
 pip install numpy pandas tensorflow scikit-learn matplotlib pillow openpyxl scrapy selenium Scrapy requests beautifulsoup4 flask pygame opencv-python
